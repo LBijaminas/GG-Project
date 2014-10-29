87d7687bc8ec8519094f6850bc40afdda1247d21
LeeGiS <lukas_dmx@yahoo.com>
2014-10-28 21:05:09 -0400

    Refactored

--- app/src/main/java/com/example/lukas/googleglassproject/Compass.java
+++ app/src/main/java/com/example/lukas/googleglassproject/Compass.java
@@ -4,13 +4,16 @@ package com.example.lukas.googleglassproject;
  * Created by lukas on 10/17/14.
  */
 
-import android.content.Context;
 import android.hardware.GeomagneticField;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
+
+/*
+ * TODO: Change that the degrees would be from 0 to 360 instead of -180 to 180. But do we really need that?
+ */
 
 public class Compass {
 
@@ -23,48 +26,58 @@ public class Compass {
      */
     private static final int ARM_DISPLACEMENT_DEGREES = 6;
 
-    private final SensorManager mSensorManager; // to get the sensor manager
-    private final float[] mRotationMatrix;  // to compute the rotation matrix
-    private final float[] mOrientation; // to compute the orientation
+    private final SensorManager sensorManager; // to get the sensor manager
+    private final float[] rotationMatrix;  // to compute the rotation matrix
+    private final float[] orientation; // to compute the orientation
 
-    private float mHeading;
-    private float mPitch;
+    private float degrees;
     private GeomagneticField mGeomagneticField;
 
-    private SensorEventListener mSensorListener = new SensorEventListener() {
+    // create the event handler
+    private SensorEventListener sensorListener = new SensorEventListener() {
 
-        // default function that has to be implemented
+        // default function that has to be implemented, but we don't need it at this point
         @Override
         public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 
+        /**
+         * The function handles the sensor measurement change
+         *
+         * @param event the Sensor event that has happened
+         */
         @Override
         public void onSensorChanged(SensorEvent event) {
+
+            // we only care about the rotation about our axis, so let's
+            // see if the rotation happened
             if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
-                // Get the current heading from the sensor, then notify the listeners of the
-                // change.
-                SensorManager.getRotationMatrixFromVector(mRotationMatrix, event.values);
-                SensorManager.remapCoordinateSystem(mRotationMatrix, SensorManager.AXIS_X,
-                        SensorManager.AXIS_Z, mRotationMatrix);
-                SensorManager.getOrientation(mRotationMatrix, mOrientation);
 
-                // Store the pitch (used to display a message indicating that the user's head
-                // angle is too steep to produce reliable results.
-                mPitch = (float) Math.toDegrees(mOrientation[1]);
+                // get the rotation matrix and put it into our variable
+                SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values);
 
-                // Convert the heading (which is relative to magnetic north) to one that is
-                // relative to true north, using the user's current location to compute this.
-                float magneticHeading = (float) Math.toDegrees(mOrientation[0]);
-                mHeading = (computeTrueNorth(magneticHeading) % 360.0f) - ARM_DISPLACEMENT_DEGREES;
-                Log.i("LUKAS", Float.toString(mHeading));
+                // remap the matrix, so that we could calculate the rotaton about the Z axis
+                SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_X,
+                        SensorManager.AXIS_Z, rotationMatrix);
+
+                // compute the device orientation based on the rotation matrix
+                SensorManager.getOrientation(rotationMatrix, orientation);
+
+
+                //calculate the degrees
+                degrees = (float) (Math.toDegrees(orientation[0]) % 360.0f) - ARM_DISPLACEMENT_DEGREES;
+
+
+                // log to see the output
+                Log.i("LUKAS", Float.toString(degrees));
             }
         }
 
     };
 
     public Compass(SensorManager sensorManager) {
-        this.mRotationMatrix = new float[16];
-        this.mOrientation = new float[9];
-        this.mSensorManager = sensorManager;
+        this.rotationMatrix = new float[16];
+        this.orientation = new float[9];
+        this.sensorManager = sensorManager;
     }
 
 
@@ -72,8 +85,8 @@ public class Compass {
      * In start function we merely register the event listener
      */
     public void start() {
-        mSensorManager.registerListener(mSensorListener,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),
+        this.sensorManager.registerListener(this.sensorListener,
+                this.sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),
                 SensorManager.SENSOR_DELAY_UI);
     }
 
@@ -81,7 +94,7 @@ public class Compass {
      * Unregisters the event listener
      */
     public void stop() {
-        mSensorManager.unregisterListener(mSensorListener);
+        this.sensorManager.unregisterListener(this.sensorListener);
     }
 
     /**
@@ -90,32 +103,8 @@ public class Compass {
      *
      * @return the user's current heading, in degrees
      */
-    public float getHeading() {
-        return mHeading;
+    public float getDegrees() {
+        return this.degrees;
     }
 
-    /**
-     * Gets the user's current pitch (head tilt angle), in degrees. The result is guaranteed to be
-     * between -90 and 90.
-     *
-     * @return the user's current pitch angle, in degrees
-     */
-    public float getPitch() {
-        return mPitch;
-    }
-
-    /**
-     * Use the magnetic field to compute true (geographic) north from the specified heading
-     * relative to magnetic north.
-     *
-     * @param heading the heading (in degrees) relative to magnetic north
-     * @return the heading (in degrees) relative to true north
-     */
-    private float computeTrueNorth(float heading) {
-        if (mGeomagneticField != null) {
-            return heading + mGeomagneticField.getDeclination();
-        } else {
-            return heading;
-        }
-    }
 }
