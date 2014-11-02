140d440f8737f3d7842b823d2d561b1d534b21fb
Hugo Torres <hugo@Hugos-MacBook-Air.local>
2014-11-02 10:21:15 -0500

    Implemented Locator service; still needs intents from main activity for superclass constructor and destructor

--- app/src/main/java/edu/barry/seminar/glass/Locator.java
+++ app/src/main/java/edu/barry/seminar/glass/Locator.java
@@ -3,8 +3,16 @@ package edu.barry.seminar.glass;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
 
 public class Locator extends Service implements
         GooglePlayServicesClient.ConnectionCallbacks,
@@ -16,7 +24,6 @@ public class Locator extends Service implements
     public void onConnected(Bundle dataBundle) {
         // Display the connection status
         Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-
     }
 
     @Override
@@ -27,42 +34,12 @@ public class Locator extends Service implements
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        /*
-         * Google Play services can resolve some errors it detects.
-         * If the error has a resolution, try sending an Intent to
-         * start a Google Play services activity that can resolve
-         * error.
-         */
-        if (connectionResult.hasResolution()) {
-            try {
-                // Start an Activity that tries to resolve the error
-                connectionResult.startResolutionForResult(
-                        this,
-                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
-                /*
-                 * Thrown if Google Play services canceled the original
-                 * PendingIntent
-                 */
-            } catch (IntentSender.SendIntentException e) {
-                // Log the error
-                e.printStackTrace();
-            }
-        } else {
-            /*
-             * If no resolution is available, display a dialog to the
-             * user with the error.
-             */
-            showErrorDialog(connectionResult.getErrorCode());
-        }
-    }
+    public void onConnectionFailed(ConnectionResult connectionResult) {}
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         mLocationClient = new LocationClient(this, this, this);
     }
 
-    @Override
     protected void onStart() {
         super.onStart();
         // Connect the client.
@@ -76,5 +53,10 @@ public class Locator extends Service implements
         super.onStop();
     }
 
+    @Override
+    public IBinder onBind(Intent i){
+
+    }
+    LocationClient mLocationClient;
     Location mCurrentLocation = mLocationClient.getLastLocation();
 }
