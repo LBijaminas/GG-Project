6dc1174208c0786424a243d1ad9a59f4a201f884
Hugo Torres <hugo.torres1993@gmail.com>
2014-11-03 16:53:35 -0500

    Changed Locator to use LocationManager created from context's location service, whatever that may be

--- app/src/main/java/edu/barry/seminar/glass/Locator.java
+++ app/src/main/java/edu/barry/seminar/glass/Locator.java
@@ -3,53 +3,38 @@ package edu.barry.seminar.glass;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient;
-import com.google.android.gms.location.LocationClient;
-
-public class Locator extends Service implements
-        GooglePlayServicesClient.ConnectionCallbacks,
-        GooglePlayServicesClient.OnConnectionFailedListener {
-    LocationClient mLocationClient;
+public class Locator extends Service {
+    Location l;
+    LocationManager locationManager;
 
     public Locator() {
     }
 
-    @Override
-    public void onConnected(Bundle dataBundle) {
-        // Display the connection status
-        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-
-        Location mCurrentLocation = mLocationClient.getLastLocation();
-        Log.i("DEBUG", mCurrentLocation.toString());
-    }
-
-    @Override
-    public void onDisconnected() {
-        // Display the connection status
-        Toast.makeText(this, "Disconnected. Please re-connect.",
-                Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {}
-
     protected void onCreate(Bundle savedInstanceState) {
-        mLocationClient = new LocationClient(this, this, this);
-    }
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
-    public void onStart(Intent intent, int i) {
-        super.onStart(intent, i);
-        // Connect the client.
-        mLocationClient.connect();
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                l = location;
+                Log.i("DEBUG", l.toString());
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
+                locationListener);
     }
 
     @Override
